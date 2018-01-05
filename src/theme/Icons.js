916ab085c7309dcc08f5349b15e2f1fbaebe8b7c
Baymax <devbaymax@gmail.com>
2018-01-06 03:46:46 +0400

    forth commit

---
+++ src/theme/Icons.js
@@ -0,0 +1,23 @@
+import EntypoIcons from 'react-native-vector-icons/Entypo';
+import EvilIconsIcons from 'react-native-vector-icons/EvilIcons';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import FoundationIcons from 'react-native-vector-icons/Foundation';
+import IoniconsIcons from 'react-native-vector-icons/Ionicons';
+import MaterialIconsIcons from 'react-native-vector-icons/MaterialIcons';
+import OcticonsIcons from 'react-native-vector-icons/Octicons';
+import ZocialIcons from 'react-native-vector-icons/Zocial';
+import MaterialCommunityIconsIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Icons = {
+  Entypo: EntypoIcons,
+  EvilIcons: EvilIconsIcons,
+  FontAwesome: FontAwesomeIcons,
+  Foundation: FoundationIcons,
+  Ionicons: IoniconsIcons,
+  MaterialIcons: MaterialIconsIcons,
+  Octicons: OcticonsIcons,
+  Zocial: ZocialIcons,
+  MaterialCommunityIcons: MaterialCommunityIconsIcons,
+};
+
+export default Icons;
--- src/theme/index.js
+++ src/theme/index.js
@@ -3,5 +3,6 @@ import Fonts from './Fonts';
 import Metrics from './Metrics';
 import Images from './Images';
 import Styles from './Styles';
+import Icons from './Icons';
 
-export { Colors, Fonts, Images, Metrics, Styles };
+export { Colors, Fonts, Images, Metrics, Styles, Icons };
